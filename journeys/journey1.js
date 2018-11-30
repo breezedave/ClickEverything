15a34d96bc39f90393136eca847308a2629dc591
Dave Breeze <breezedave@googlemail.com>
2018-11-30 10:13:25 +0000

    Revert back to sushi site

--- journeys/journey1.js
+++ journeys/journey1.js
@@ -1,9 +1,8 @@
 const actions = [
-    ["goto", "http://localhost:5011/dashboard"]
-    , ["waitFor", 2000]
-    , ["click", ".FilterControl"]
-    , ["snap"]
-    , ["clickAllTheThings"]
+    ["goto", "https://demos.telerik.com/kendo-ui/websushi/"],
+    ["waitFor", 2000],
+    ["snap"],
+    ["clickAllTheThings"]
 ]
 
 const journey = {
@@ -12,3 +11,12 @@ const journey = {
 }
 
 module.exports = journey;
+
+
+/*const actions = [
+    ["goto", "http://localhost:5011/dashboard"]
+    , ["waitFor", 2000]
+    , ["click", ".FilterControl"]
+    , ["snap"]
+    , ["clickAllTheThings"]
+]*/
